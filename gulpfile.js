df2dcfc039cdb9d33748efe0051dc287101cfaac
mangesh1234 <mtembhurne4@gmail.com>
2017-05-26 17:31:11 +0530

    add gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,37 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+// Gulpfile.js
 
-var gulp = require('gulp');
+'use strict';
 
-gulp.task('default', function() {
-  // place code for your default task here
+const gulp = require('gulp'),
+  nodemon = require('gulp-nodemon'),
+  eslint = require('gulp-eslint');
+
+gulp.task('lint', function() {
+  // ESLint ignores files with 'node_modules' paths.
+  // So, it's best to have gulp ignore the directory as well.
+  // Also, Be sure to return the stream from the task;
+  // Otherwise, the task may end before the stream has finished.
+  return gulp.src(['./**/*.js', '!node_modules/**', '!coverage/**'])
+    // eslint() attaches the lint output to the 'eslint' property
+    // of the file object so it can be used by other modules.
+    .pipe(eslint())
+    // eslint.format() outputs the lint results to the console.
+    // Alternatively use eslint.formatEach() (see Docs).
+    .pipe(eslint.format())
+    // To have the process exit with an error code (1) on
+    // lint error, return the stream and pipe to failAfterError last.
+    .pipe(eslint.failAfterError());
+});
+
+gulp.task('serve', function() {
+  // process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+  const nodemonConfig = {
+    script: 'main.js',
+    ext: 'js',
+    tasks: ['lint']
+  };
+  nodemon(nodemonConfig)
+    .on('restart', function() {
+      // console.log('restarted!');
+    });
 });
